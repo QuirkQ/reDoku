610a105b4b103c5e055de2dd1ff9f21111c89b85
Quint Pieters <47210781+QuirkQ@users.noreply.github.com>
2026-08-20 22:06:44 +0200

    build: armv7hf cross target for the reMarkable 2
    Cross builds get no platform port by default (lib/mruby/build.rb
    effective_ports returns [] for MRuby::CrossBuild), leaving mruby-io/
    mruby-dir HAL symbols (mrb_hal_io_*, mrb_hal_dir_*) undefined at link
    time. Added conf.ports :posix since the device is ARM Linux.
    
    Also gitignore build_config.rb.lock, which rake drops in the repo
    root on every build.

--- build_config.rb
+++ build_config.rb
@@ -9,3 +9,32 @@ MRuby::Build.new do |conf|
   conf.enable_debug
   conf.enable_test
 end
+
+# Device build: armv7hf cross-compile for the reMarkable 2 (firmware >= 3.18,
+# glibc-compatible per PLAN.md §5). Produces bin/mruby for on-device use.
+MRuby::CrossBuild.new('rm2') do |conf|
+  conf.toolchain :gcc
+
+  tc = '/opt/x-tools/arm-remarkable-linux-gnueabihf/bin/arm-remarkable-linux-gnueabihf'
+  conf.cc do |cc|
+    cc.command = "#{tc}-gcc"
+    cc.flags << %w[-march=armv7-a -mfpu=neon -mfloat-abi=hard -O2]
+  end
+  conf.linker do |linker|
+    linker.command = "#{tc}-gcc"
+  end
+  conf.archiver do |archiver|
+    archiver.command = "#{tc}-ar"
+  end
+
+  conf.gembox 'default'
+
+  # Cross builds select NO platform port by default (lib/mruby/build.rb
+  # effective_ports returns [] for MRuby::CrossBuild), which leaves
+  # mruby-io/mruby-dir HAL symbols (mrb_hal_io_*, mrb_hal_dir_*) undefined
+  # at link time. The device is ARM Linux, so the posix port is correct.
+  conf.ports :posix
+
+  conf.build_mrbtest_lib_only
+  conf.disable_cxx_exception
+end
